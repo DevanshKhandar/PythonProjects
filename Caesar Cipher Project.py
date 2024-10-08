996ed18d342d56f0e91fddbf879da886ad885952
DevanshKhandar <162901376+DevanshKhandar@users.noreply.github.com>
2024-10-08 23:15:26 +0530

    Add files via upload

--- Caesar Cipher Project.py
+++ Caesar Cipher Project.py
@@ -1,6 +1,6 @@
-import art
+import Caesar_cipher_project_art
 
-print(art.logo)
+print(Caesar_cipher_project_art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
